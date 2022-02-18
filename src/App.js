378b71754b91d8e17cfdb16e9540087bd1f87ad6
evxeen <evxeen@gmail.com>
2022-02-18 19:14:15 +0300

    feat: check the guest list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+
+import {GUESTS_API, DIETS_API} from "./config";
+import {getData, changeStrings, grouping} from "./helpers";
 
 function App() {
+  const [guestList, setGuestList] = useState([]);
 
-  const checkStore = () => {
-    if (localStorage.length > 0) {
-
+  const checkStore = async () => {
+    if (localStorage.length === 0) {
+      const { party } = await getData(GUESTS_API);
+      const { diet } = await getData(DIETS_API, changeStrings(party));
+      setGuestList(grouping(party, diet));
     }
   }
 
   useEffect(() => {
     checkStore();
   }, [])
+
+  console.log(guestList);
   return (
     <div className="App">
       <h1>Loading...</h1>
---
+++ src/helpers.js
@@ -0,0 +1,37 @@
+export const changeStrings = (party) => {
+  let result = [];
+  party.forEach((person) => {
+    result.push(`${person.name.replace(' ' , '%20')}`);
+  })
+  return result.join();
+}
+
+export const getData = (api, str = '') => {
+   return fetch(`${api}${str}`).then(res => res.json());
+}
+
+export const getPizza = (data) => {
+  const vegans = data.filter(person => person.isVegan).length;
+
+  function getPercent() {
+    const percent = Math.floor((vegans / data.length) * 100);
+    if (percent > 51) {
+      return 'vegan'
+    }
+    return 'meat'
+  }
+
+  return `${getPercent()}/${vegans}`;
+}
+
+export const grouping = (party, diet) => {
+  const whoEats = party.filter(members => members.eatsPizza);
+
+  for (let i = 0; i < whoEats.length; i++) {
+    if (whoEats[i].name) {
+      whoEats[i].isVegan = diet[i].isVegan
+    }
+  }
+
+  return whoEats
+}
