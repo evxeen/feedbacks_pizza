34de55f77f2e12e474f773d4c024b72beead289f
evxeen <evxeen@gmail.com>
2022-02-18 18:36:35 +0300

    init

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const GUESTS_API = 'https://gp-js-test.herokuapp.com/pizza/guests';
+export const DIETS_API = 'https://gp-js-test.herokuapp.com/pizza/world-diets-book/';
+export const ORDER_API = 'https://gp-js-test.herokuapp.com/pizza/order/';
+export const CURRENCY_API = 'https://gp-js-test.herokuapp.com/pizza/currency';
